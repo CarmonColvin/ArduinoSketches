87e15c654621603de4ac1872c84fc4e80993e046
Carmon Colvin <9137759+CarmonColvin@users.noreply.github.com>
2020-11-14 21:59:41 -0600

    Setup using sendDataUntil for AT setup commands; Added RED, GREEN led lights; Updated method of calculating broadcast frequency; Code cleanup;

--- DatumGarageNBP/DatumGarageNBP.ino
+++ DatumGarageNBP/DatumGarageNBP.ino
@@ -1,28 +1,13 @@
-/*
-  1. Upload this code and open the serial monitor
-  2. On the serial monitor enter the command AT
-  3. If the response is OK, your esp8266 serial baud rate is 115200.
-    Close this sketch and run the example code.
-    Then on the void setup make this change: espSerial.begin(115200); and upload the code.
-
-    If you want to change the speed to 9600 use the command AT+UART_CUR=9600,8,1,0,0
-
-  4. Otherwise if you can't see anything on the serial display reset the ESP8266 module
-    change the esp8266 serial baud rate to 9600 or other, until you get a response.
-    Then enter the command AT+UART_CUR=115200,8,1,0,0 to change the speed to 115200
-
-*/
-
-// AT                             Command to display the OK message
-// AT+GMR                         Command to display esp8266 version info
-// AT+UART_CUR=9600,8,1,0,0       Command to change the ESP8266 serial baud rate  to 9600
-// AT Command to change the ESP8266 serial baud rate  to 115200   AT+UART_CUR=115200,8,1,0,0
-
 #include <max6675.h>
 
 const bool DEBUG = false;
-const String versionString = "v0.25";
+const String versionString = "v0.28";
 
+// Define LED Pins
+const int RED = 5;
+const int GREEN = 6;
+
+// Arduino pins for MAX6675 Thermocouple board
 const int A_thermo_gnd_pin = 45;
 const int A_thermo_vcc_pin = 47;
 const int A_thermo_so_pin  = 49;
@@ -45,25 +30,38 @@ unsigned long now = millis();
 unsigned long lastNow = 0;
 
 void setup() {
+  // Setup and turn on the LED lights
+  pinMode(RED, OUTPUT);
+  pinMode(GREEN, OUTPUT);
+
+  digitalWrite(RED, HIGH);
+  digitalWrite(GREEN, HIGH);
+
   Serial1.begin(115200);
   Serial.begin(9600);
 
-  // Setup the max6675 thermocuple module
+  // Setup the max6675 thermocuple module.
   pinMode(A_thermo_vcc_pin, OUTPUT);
   pinMode(A_thermo_gnd_pin, OUTPUT);
   digitalWrite(A_thermo_vcc_pin, HIGH);
   digitalWrite(A_thermo_gnd_pin, LOW);
 
+  Serial.println("Datum Garage NBP Streamer " + versionString);
+
   sampleStaticJoystick();
 
-    // Announce yourself!
-    Serial.println("Datum Garage NBP Streamer " + versionString);
+  // Setup the AT communication.
+  sendDataUntil("AT+RST\r\n", 3000, "ready", "reset");
+  // sendData("AT+UART_CUR=9600,8,1,0,0", 1250); // set ESP8266 baud rate to 9600
+  // sendData("AT+UART_CUR=115200,8,1,0,0", 1250); // set ESP8266 baud rate to 115200
+  // sendDataUntil("AT+CIFSR\r\n", 3000, "OK", "ip-address"); // query local IP address
+   sendDataUntil("AT+CIPMUX=1\r\n", 3000, "OK", "multi-mode"); // set multi mode
+   sendDataUntil("AT+CIPSERVER=1,80\r\n", 3000, "OK", "cip-server"); // setup server
 
-  // Setup the AT communication. Is all of this even necessary?
-  sendData("AT+RST\r\n", 1250); // reset
-//  sendData("AT+CIFSR\r\n", 1250); // query local IP address
-  sendData("AT+CIPMUX=1\r\n", 1250); // set multi mode
-  sendData("AT+CIPSERVER=1,80\r\n", 1250); // setup server
+   Serial.println("Server configuration complete.");
+
+  digitalWrite(RED, LOW);
+  digitalWrite(GREEN, LOW);
 }
 
 // Use these variables to count the number of broadcasts made each second.
@@ -71,12 +69,27 @@ float broadcastFrequency = 0.0;
 unsigned int broadcastFrequencyCounter = 0;
 unsigned long lastBroadcastFrequencyTime = 0;
 
+const int frequencyHistoryDepth = 40;
+float frequencyHistory[frequencyHistoryDepth];
+
+float calculateFrequency() {
+    float currentFrequency = 1000.0 / (now - lastNow);
+    float frequencyHistoryTotal = currentFrequency;
+
+      for (int idx = frequencyHistoryDepth - 1; idx > 0; idx--){
+        frequencyHistory[idx] = frequencyHistory[idx-1];
+        frequencyHistoryTotal += frequencyHistory[idx-1];
+      }
+      frequencyHistory[0] = currentFrequency;
+      float averageFreq = frequencyHistoryTotal / frequencyHistoryDepth;
+      return averageFreq;
+}
+
 void loop() {
   now = millis();
 
-  // This is not / by 0 safe. Could happen
-  // todo KEEP track of last few cycles and average
-  broadcastFrequency = 1000.0 / (now - lastNow);
+  float broadcastFrequency = calculateFrequency();
+  
   lastNow = now;
   
   // Build a string in the NBP v1.0 format
@@ -84,7 +97,7 @@ void loop() {
   toBroadcast += "\"Ambient Temp\",\"C\":" + String(GetTemperatureA(), 2) + "\n";
   toBroadcast += "\"Movement X\",\"MM\":" + String((analogRead(X_pin) - Static_X) / MM_calib, 3) + "\n";
   toBroadcast += "\"Movement Y\",\"MM\":" + String((analogRead(Y_pin) - Static_Y) / MM_calib, 3) + "\n";
-  toBroadcast += "\"Broadcast Freq.\",\"Hz\":" + String(broadcastFrequency, 2) + "\n";
+  toBroadcast += "\"Broadcast Freq.\",\"Hz\":" + String(broadcastFrequency, 1) + "\n";
   toBroadcast += "#\n";
 
   // Append the metadata if enough time has passed. Target is every 5 seconds.
@@ -95,14 +108,14 @@ void loop() {
     toBroadcast += "@VERSION:" + versionString + "\n";
   }
 
-    // Build the AT CIPSEND command line here.
+    // Build the AT CIPSEND command line.
     String atCipSendCommand = "AT+CIPSEND=0," + String(toBroadcast.length()) + "\r\n";
 
     // Send the command.
-    sendDataUntil(atCipSendCommand, 250, ">", "SEND-COMMAND");
+    sendDataUntil(atCipSendCommand, 200, ">", "SEND-COMMAND");
 
     // Send the string to broadcast.
-    sendDataUntil(toBroadcast, 250, "SEND OK", "SEND-PAYLOAD");
+    sendDataUntil(toBroadcast, 200, "SEND OK", "SEND-PAYLOAD");
 }
 
 void sendDataUntil(String command, const int timeout, String expected, String tracer){
@@ -114,13 +127,13 @@ void sendDataUntil(String command, const int timeout, String expected, String tr
 
   while (keepTrying && (time + timeout) > millis()) {
     while (keepTrying && Serial1.available()) {
-      char c = Serial1.read(); // read the next character.
+      char c = Serial1.read();
       response += c;
       keepTrying = !response.endsWith(expected) || response.endsWith("ERROR");
     }
   }
   if (DEBUG || response.indexOf("ERROR") > 0) {
-    Serial.println(tracer + " || " + response + " ||");
+    Serial.println(tracer + " || " + String(millis()-time) + "ms || " + response.endsWith(expected) + " || " + response + " ||");
   }
 }
 
